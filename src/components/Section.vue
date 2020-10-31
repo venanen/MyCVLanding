e1d8af9baf7d87ca73844d9096ca1c7f8e7d3222
venanen <help.venanen@yandex.ru>
2020-10-31 23:10:50 +0300

    add store

---
+++ src/components/Section.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <div id="sectionBackground">
+      <slot name="background">
+      </slot>
+    </div>
+    <slot>
+
+    </slot>
+  </section>
+
+</template>
+
+<script>
+export default {
+  name: "Section"
+}
+</script>
+
+<style scoped>
+
+</style>
