d5845dcb5c5a45cfd33d5dd811e54e9c364081b1
Иван Викторович <help.venanen@yandex.ru>
2020-12-16 18:03:23 +0300

    fix contacts

--- src/data/contacts.js
+++ src/data/contacts.js
@@ -1,7 +1,7 @@
 export default [//TODO add icons 
     {name: "GitHub", link: "https://github.com/venanen"},
     {name: 'Habr Career', link: 'https://career.habr.com/venanen'},
-    {name: 'HedHunter', link: 'https://hh.ru/resume/26cab889ff072647470039ed1f724852656368'},
+    {name: 'HeadHunter', link: 'https://hh.ru/resume/26cab889ff072647470039ed1f724852656368'},
     {name: "Linkdin", link: "https://www.linkedin.com/mwlite/in/isaev-ivan-2a0000200"},
     {name: "MDN", link: "https://developer.mozilla.org/ru/profiles/venanen"},
     {name: "Instagram", link: "https://instagram.com/___just__nick___"},
